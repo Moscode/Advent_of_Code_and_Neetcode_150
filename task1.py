48bff574d0f1f2310ee218b0dd8e17f3b6ddc77e
Odunosh Moses <Odunoshomoses@gmail.com>
2024-01-01 23:04:39 +0000

    task two of problem day 1 solved

--- task1.py
+++ task1.py
@@ -3,9 +3,12 @@ D = open(sys.argv[1]).read().strip()
 ans = 0
 for line in D.split('\n'):
     digits = []
-    for c in line:
+    for i, c in enumerate(line):
         if c.isdigit():
             digits.append(c)
+        for d, val in enumerate(['one','two','three','four','five','six','seven','eight','nine']):
+            if line[i:].startswith(val):
+                digits.append(str(d+1))
     score = int(digits[0]+digits[-1])
     ans += score
 print(ans)
