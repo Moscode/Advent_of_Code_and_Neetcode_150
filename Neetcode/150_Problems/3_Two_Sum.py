bbe1900d9112d8cfa070d6c9813dda55634af5a6
Odunosho Moses <odunoshomoses@gmail.com>
2024-02-22 04:34:02 +0100

    solved two sums using both naive approach and optimize solution

--- Neetcode/150_Problems/3_Two_Sum.py
+++ Neetcode/150_Problems/3_Two_Sum.py
@@ -1,3 +1,4 @@
+#Naive Solution
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         start = 0
@@ -8,4 +9,17 @@ class Solution:
                     return [start, j]
                 j += 1
             start += 1
+        return []
+
+
+#Optimize Solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numsMap = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numsMap:
+                return [numsMap[complement], i]
+            numsMap[nums[i]] = i
         return []
